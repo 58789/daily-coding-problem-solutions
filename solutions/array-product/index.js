a3379596093fb54ca095bd309d01a1bd72fd854d
Henry Williams <3443648+henryjw@users.noreply.github.com>
2019-05-04 13:49:36 -0400

    array-product without division

--- solutions/array-product/index.js
+++ solutions/array-product/index.js
@@ -1,4 +1,38 @@
-const solve = (input = [1]) => {
+const self = module.exports
+
+const reverse = array => array.slice().reverse()
+
+/**
+ * 
+ * @param {Array<number>} input 
+ * @returns {Number} Product
+ */
+const product = (input = [1]) => {
+    const fn = (result, value) => result * value
+    return input.reduce(fn, 1)
+}
+
+const buildProductsArray = ({ input = [1], reversed = false }) => {
+    const results = new Array(input.length)
+
+    if (reversed) {
+        for(let i = input.length - 1; i >= 0; i--) {
+            const previous = results[i + 1] || 1
+            const current = input[i]
+            results[i] = previous * current
+        }
+    } else {
+        for(let i = 0; i < input.length; i++) {
+            const previous = results[i - 1] || 1
+            const current = input[i]
+            results[i] = previous * current
+        }
+    }
+
+    return results
+}
+
+self.solve = (input = [1]) => {
     if (input.length < 2) {
         throw new Error('Input size must be at least 2')
     }
@@ -8,10 +42,11 @@ const solve = (input = [1]) => {
     return input.map(value => totalProduct / value)
 }
 
-const product = (input = [1]) => {
-    const fn = (result, value) => result * value
-    return input.reduce(fn, 1)
+self.solveNoDivision = (input = []) => {
+    const prefix = buildProductsArray({ input, reversed: false })
+    const postfix = buildProductsArray({ input, reversed: true })
+
+    const calculateProduct = index => (prefix[index - 1] || 1) * (postfix[index + 1] || 1)
+
+    return input.map((value, index) => calculateProduct(index))
 }
-
-
-module.exports = solve
--- test/array-product.spec.js
+++ test/array-product.spec.js
@@ -1,24 +1,32 @@
 const { expect } = require('chai')
 
-const solve = require('../solutions/array-product')
+const solver = require('../solutions/array-product')
 
+const tester = (solve) => (input, expected) => {
+    const actual = solve(input)
+    expect(actual, `input: ${input.join(', ')}`).to.deep.equal(expected)
+}
 
-describe('Array Product', function() {
+const runTests = (test) => {
     it('returns correct result', () => {
         test([1, 2, 3, 4, 5], [120, 60, 40, 30, 24])
         test([3, 2, 1], [2, 3, 6])
     })
 
-    it('handles 0 in input', () => {
-        test([3, 2, 1, 0], [0, 0, 0, 6])
-    })
-
     it('throws error if input size is < 2', () => {
         expect(() => solve([1])).to.throw();
     })
-})
-
-const test = (input, expected) => {
-    const actual = solve(input)
-    expect(actual, `input: ${input.join(', ')}`).to.deep.equal(expected)
 }
+
+
+describe('Array Product', function() {
+    describe('With Division', () => {
+        const test = tester(solver.solve)
+        runTests(test)
+    })
+
+    describe('Without Division', () => {
+        const test = tester(solver.solveNoDivision)
+        runTests(test)
+    })
+})
